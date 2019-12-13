eecd0f201bcf007e0650581588e093d57e5a1ed3
Patrick Petermair <patrick.petermair@gmail.com>
2019-12-13 18:16:13 +0100

    day 2

--- day2/solution.py
+++ day2/solution.py
@@ -4,38 +4,49 @@ from os import path
 
 
 def main():
-    program = readInput()
-    position = 0;
-    while position < len(program):
-        operation = program[position]
+    memory = read_input()
+    result_part1 = run_program(memory, 12, 2)
+    result_part2 = 0
+
+    for noun in range(100):
+        for verb in range(100):
+            if run_program(read_input(), noun, verb) == 19690720:
+                result_part2 = 100 * noun + verb
+
+    print(result_part1)
+    print(result_part2)
+
+
+def run_program(memory, noun, verb):
+    memory[1] = noun
+    memory[2] = verb
+    position = 0
+
+    while position < len(memory):
+        operation = memory[position]
         if operation == 99:
             break
         elif operation == 1:
-            operand1 = program[program[position+1]]
-            operand2 = program[program[position+2]]
-            result = operand1 + operand2
-            location = program[position+3]
-            program[location] = result
+            operand1 = memory[memory[position + 1]]
+            operand2 = memory[memory[position + 2]]
+            location = memory[position + 3]
+            memory[location] = operand1 + operand2
         elif operation == 2:
-            operand1 = program[program[position + 1]]
-            operand2 = program[program[position + 2]]
-            location = program[position + 3]
-            result = operand1 * operand2
-            program[location] = result
+            operand1 = memory[memory[position + 1]]
+            operand2 = memory[memory[position + 2]]
+            location = memory[position + 3]
+            memory[location] = operand1 * operand2
         else:
             raise ValueError('Unknown opcode')
 
         position += 4
 
-    print(program[0])
+    return memory[0]
 
-def readInput():
+
+def read_input():
     with open(path.join(path.dirname(__file__), "input")) as f:
-        input = list(map(int,(f.readline().strip().split(","))))
-        input[1] = 12
-        input[2] = 2
-        return input
-
+        return list(map(int,(f.readline().strip().split(","))))
 
 
 if __name__ == "__main__":
